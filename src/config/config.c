afbce7de05f562c3ce602e4354c993dfc7a9db30
Martez Reed <martez.reed@greenreedtech.com>
2023-07-09 21:57:38 -0500

    Update config testing

--- src/config/config.c
+++ src/config/config.c
@@ -331,9 +331,9 @@ REQUIRE_OBJECT ( pci_settings );
 #ifdef VMWARE_SETTINGS
 REQUIRE_OBJECT ( guestinfo );
 #endif
-#ifdef XEN_SETTINGS
-REQUIRE_OBJECT ( xenstore );
-#endif
+//#ifdef XEN_SETTINGS
+//REQUIRE_OBJECT ( xenstore );
+//#endif
 #ifdef CPUID_SETTINGS
 REQUIRE_OBJECT ( cpuid_settings );
 #endif
