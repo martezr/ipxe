5e871f94c1720fc39732ddc4f80e6ccc396fc5ee
Martez Reed <martez.reed@greenreedtech.com>
2023-07-09 23:11:24 -0500

    More debugging

--- src/config/general.h
+++ src/config/general.h
@@ -152,8 +152,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#define PCI_CMD		/* PCI commands */
 //#define PARAM_CMD		/* Request parameter commands */
 //#define NEIGHBOUR_CMD		/* Neighbour management commands */
-//#define PING_CMD		/* Ping command */
-//#define CONSOLE_CMD		/* Console command */
+#define PING_CMD		/* Ping command */
+#define CONSOLE_CMD		/* Console command */
 //#define IPSTAT_CMD		/* IP statistics commands */
 //#define PROFSTAT_CMD		/* Profiling commands */
 //#define NTP_CMD		/* NTP commands */
